4107436c787bedac0b77dd3076771f750321f99f
Juan Miguel <juanmiber@amazon.com>
2023-07-25 16:15:16 -0500

    Se crea request para determinar el clima en valledupar

--- saludador.py
+++ saludador.py
@@ -1,8 +1,18 @@
-import json
+import os
+import requests
 
+url = os.getenv("WEATHER_URL", "https://weatherapi-com.p.rapidapi.com/current.json")
+querystring = {"q":"10.46, -73.25"}
+headers = {
+    "X-RapidAPI-Key": "SIGN-UP-FOR-KEY",
+    "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com"
+}
 
 def handler(event, context):
+
+    response = requests.get(url, headers=headers, params=querystring)
+
     return {
         'statusCode': 200,
-        'body': 'Un saludo al Salvi'
+        'body': response.json()
     }
